4699838a072c9a5a0162f045c9919513309f714e
Thomas yang <168447636@qq.com>
2018-09-17 15:24:05 +0800

    1

--- man.linuxde.net/cmd_down.sh
+++ man.linuxde.net/cmd_down.sh
@@ -1,2 +1,2 @@
 for i in `cat cmd.txt`; do echo "wget man.linuxde.net/$i;mv $i $i.html"; done|sh
-sed -i 's/man.linuxde.net/man.yunwei.edu/g' *html
+sed -i 's/man.linuxde.net/Thomas-YangHT.github.io/man.linuxde.net/g' *html
--- man.linuxde.net/wp-content/themes/pc_mobile/js/common.js
+++ man.linuxde.net/wp-content/themes/pc_mobile/js/common.js
@@ -1,11 +1,11 @@
 ﻿$(document).ready(function(){
 	//添加广告
 	(function(){
-		$("#ad-arc-top").html('' +
-			'<a href="http://www.magedu.com/74163.html?linux_mingling_banner" target="_blank">' +
-				'<img src="/wp-content/uploads/ad/magedu_arc_top.png" alt="文章顶部广告" />' +
-			'</a>' +
-			'<p><a href="http://www.linuxprobe.com/chapter-00.html" target="_blank" style="color: #D33428;">《Linux就该这么学》是一本基于最新Linux系统编写的入门必读书籍，内容面向零基础读者，由浅入深渐进式教学，销量保持国内第一，年销售量预期超过10万本。<span style="color:#08c;">点此免费在线阅读。</span></a></p>'
+	//	$("#ad-arc-top").html('' +
+	//		'<a href="http://www.magedu.com/74163.html?linux_mingling_banner" target="_blank">' +
+	//			'<img src="/wp-content/uploads/ad/magedu_arc_top.png" alt="文章顶部广告" />' +
+	//		'</a>' +
+	//		'<p><a href="http://www.linuxprobe.com/chapter-00.html" target="_blank" style="color: #D33428;">《Linux就该这么学》是一本基于最新Linux系统编写的入门必读书籍，内容面向零基础读者，由浅入深渐进式教学，销量保持国内第一，年销售量预期超过10万本。<span style="color:#08c;">点此免费在线阅读。</span></a></p>'
 		);
 	})();
 
