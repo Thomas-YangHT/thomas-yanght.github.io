a76a2fd2d3de16ea311f965162e4cc9a61002e1a
root <168447636@qq.com>
2018-09-18 17:22:22 +0800

    2

--- man.linuxde.net/gen_index3.sh
+++ man.linuxde.net/gen_index3.sh
@@ -6,10 +6,10 @@ COL=10
 j=1
 k=1
 echo "<table  BORDER=1 WIDTH='100%'>  "  
-for  i in `ls *html|sed 's#.html##g'`
+for  i in `cd /usr/share/nginx/html/man.linuxde.net/;ls *html|sed 's#.html##g'`
 do 
       [[ $j -eq 1 ]] && echo "<tr>" 
-      echo "<td><a href=\"man.linuxde.net/$i.html\">$k-$i</a></td>"
+      echo "<td><a href=\"$i.html\">$k-$i</a></td>"
       [[ $j -eq $COL ]] && echo "</tr>" && j=0
       ((j++)) 
       ((k++))
--- man.linuxde.net/replace.sh
+++ man.linuxde.net/replace.sh
@@ -1,4 +1,5 @@
+  cd /usr/share/nginx/html/man.linuxde.net
   sed -i  's/<p><a.*taobaojiqiao.*非常夸张！<\/a><\/p>//g' *.html 
   sed -i  's/<li><a.*taobaojiqiao.*非常夸张！<\/a><\/li>//g' *.html 
   sed -i  's/<script.*cnzz.com.*<\/script>//g' *.html
-  sed -i 's#http://man.linuxde.net/##g' *.html 
+  sed -i  's#http://man.linuxde.net/##g' *.html 
