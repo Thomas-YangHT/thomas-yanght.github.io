b3d7b687f358edf34d48c2c15c5ce96cd8a204a6
root <168447636@qq.com>
2018-09-19 13:56:52 +0800

    2

--- man.linuxde.net/gen_index.sh
+++ man.linuxde.net/gen_index.sh
@@ -1,1 +1,17 @@
-ls *html|awk '{print "<a href=\"http://man.yunwei.edu/"$1"\">"$1" </a> </br>"}' >index.html
+#
+#ls /man.yunwei.edu/*html|awk -F'/' '{print "<a href=\"http://"$2"/"$3"\">"$3" </a> </br>"}' >/man.yunwei.edu/index.html
+#
+set +x
+COL=10
+j=1
+k=1
+echo "<table  BORDER=1 WIDTH='100%'>  "  
+for  i in `ls *html|grep -v index|sed 's#.html##g'`
+do 
+      [[ $j -eq 1 ]] && echo "<tr>" 
+      echo "<td><a href=\"$i.html\">$k-$i</a></td>"
+      [[ $j -eq $COL ]] && echo "</tr>" && j=0
+      ((j++)) 
+      ((k++))
+done
+echo "</table>"  
---
+++ man.linuxde.net/git_commit.sh
@@ -0,0 +1,5 @@
+ git pull
+ git config --global user.email "168447636@qq.com"
+ git add --all
+ git commit -m '2'
+ git push
